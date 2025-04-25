3b6f8b9bfc2d1ef5214e54bf4a70f0e85c8c3739
Morgan Saunier <morgan.saunier.gg@gmail.com>
2025-04-25 18:34:42 +0200

    edith editing

--- src/teaicher/services/text_to_speech.py
+++ src/teaicher/services/text_to_speech.py
@@ -9,8 +9,8 @@ def openai_text_to_speech(story: str, to_bytes = False, filename: str = "story.m
     # styled_input = "(Speed of speech = slow) " + story
 
     response = client.audio.speech.create(
-        # model="gpt-4o-mini-tts",
-        model="tts-1-hd",
+        model="gpt-4o-mini-tts",
+        # model="tts-1-hd",
         voice="alloy",
         input=story,
         speed=1
---
+++ uis/ui_flask/flask_2_app.py
@@ -0,0 +1,35 @@
+import os
+from dotenv import load_dotenv
+from flask import Flask, render_template, request
+
+from src.teaicher.data.get_track_duration import get_track_duration, extract_service_name
+from src.teaicher.services.get_story_length import get_user_story_length
+from src.teaicher.services.generate_story import generate_story
+from src.teaicher.services.text_to_speech import openai_text_to_speech
+from src.teaicher.services.play_audio import play_audio, play_audio_with_sync
+
+app = Flask(__name__)
+load_dotenv()
+
+@app.route('/')
+def index():
+    return render_template('index_4.html')
+
+@app.route('/generate_story', methods=['POST'])
+def generate_story_ui():
+    subject = request.form['subject']
+    user_length = int(request.form['duration'])
+    track_path = "/Users/grannygoestoheaven/code/computer science projects/teaicher/src/static/audio/Leaf_Bed.mp3"
+
+    estimated_chars = get_user_story_length(user_length)
+    with open('src/teaicher/config/patterns/insightful_brief.md', 'r') as file:
+        pattern = file.read().replace("{subject}", str(subject)).replace("{estimated_chars}", str(estimated_chars))
+
+    story, filename = generate_story(subject, pattern, estimated_chars)
+    speech_file_path = openai_text_to_speech(story)
+    play_audio_with_sync(speech_file_path, track_path)
+
+    return render_template("index_4.html", story=story, audio_link=f"/static/audio_output/{os.path.basename(speech_file_path)}")
+
+if __name__ == "__main__":
+    app.run(debug=True)

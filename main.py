a9adf919b8db7364c1df6bbeb3030d106897c4e4
Morgan Saunier <morgan.saunier.gg@gmail.com>
2026-02-09 17:28:06 +0100

    added missing '/' in css for inika font family path - replaced app.mount by app.get to include custom CORS headers

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
+
+from pathlib import Path
 
 from src.routers import ui_home, topic, story
 from src.config.settings import env_settings, load_prompts
 
 app = FastAPI()
 app.add_middleware(CORSMiddleware, allow_origins=["*"])
-app.mount("/static", StaticFiles(directory="static"), name="static")
+# app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/static/{file_path:path}")
+async def serve_static(file_path: str):
+    return FileResponse(
+        Path("static") / file_path,
+        headers={"Access-Control-Allow-Origin": "*"}
+    )
 
 app.include_router(ui_home.router)
 app.include_router(topic.router, prefix="/v1/topic")

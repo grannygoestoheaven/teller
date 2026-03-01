f45c43f101a72c349c14e89d9dee757d159cc8cb
Morgan Saunier <morgan.saunier.gg@gmail.com>
2026-03-01 15:57:37 +0100

    edited names in env_settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -54,7 +54,7 @@ class EnvSettings(BaseSettings):
     newsapi_api_key: str = Field(..., env="NEWSAPI_API_KEY")
     
     use_bucket: bool = Field(default=True, env="USE_BUCKET")  # Defaults to local
-    scw_bucket_name: str = Field(default="", env="SCW_TELLER_BUCKET_NAME")
+    scw_bucket_name: str = Field(default="", env="TELLER_BUCKET_NAME")
     scw_access_key: str = Field(default="", env="SCW_ACCESS_KEY")
     scw_secret_key: str = Field(default="", env="SCW_SECRET_KEY")
     scw_endpoint: str = Field(default="", env="TELLER_ENDPOINT")
